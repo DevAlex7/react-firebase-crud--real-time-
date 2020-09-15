c384f4c145e1f7f200358a63c6e6897a79e5a406
Alejandro gonzalez <alexgve7sv@outlook.com>
2020-09-15 00:48:41 -0600

    deleting credentials firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAz4f2dEzLBeI0ostpyCbifh6ibOu1jnbQ",
-    authDomain: "list-react-f458e.firebaseapp.com",
-    databaseURL: "https://list-react-f458e.firebaseio.com",
-    projectId: "list-react-f458e",
-    storageBucket: "list-react-f458e.appspot.com",
-    messagingSenderId: "472143037652",
-    appId: "1:472143037652:web:709cdb5bc53fed60432741",
-    measurementId: "G-0BKXRYJ7MD"
+    
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
