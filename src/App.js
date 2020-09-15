84191da5da383fd41b8e44b82b2a952241840163
Alejandro gonzalez <alexgve7sv@outlook.com>
2020-09-15 00:37:08 -0600

    initial app react with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Input from './components/input'
+import List from './components/list'
+import { db } from './services/firebase'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(()=> {
+
+    })    
+
+    const [state, setState] = useState({
+        title: '',
+        error : ''
+    })
+
+    const handleTitle = (value) => {
+        
+        if (!value) {
+            setState({
+                error : 'Ingrese un titulo'
+            })
+        } else {
+            setState({
+                title : value
+            })
+        }
+        
+    }
+
+    const createItemList = () => {
+        try {
+            const item = {
+                title : state.title,
+                complete : false
+            }
+            
+            db.push(item)
+        } catch (error) {
+            console.log(error)
+            setState({
+                error : error
+            })
+        }
+    }
+
+    return (
+        <div className="w-full h-full p-10">
+            <div className="flex flex-wrap">
+                
+                <div className="w-full md:w-1/2">
+                    <Input
+                    value={state.title}
+                    label="Agregue un item a su lista"
+                    handleAction={handleTitle}
+                    placeholder="Item"
+                    error={state.error} 
+                    />
+                    <button onClick={createItemList} className="my-2 bg-red-500 px-3 py-2 rounded-md text-white"> Ingresar </button>
+                </div>
+
+                <div className="w-full md:w-1/2">
+                    <List />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
